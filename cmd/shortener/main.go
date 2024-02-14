b5f13cc7e0698f17c0e235e65cf7393d9327cb73
@fuks.kirill <fuks.kirill@wb.ru>
2024-02-14 13:55:16 +0400

    Add env

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/FuksKS/urlshortify/internal/config"
 	"github.com/FuksKS/urlshortify/internal/handlers"
 	"net/http"
@@ -11,7 +10,6 @@ func main() {
 
 	cfg := config.InitConfig()
 
-	fmt.Println(cfg.HTTPAddr)
 	err := http.ListenAndServe(cfg.HTTPAddr, handlers.RootHandler(cfg.HTTPAddr, cfg.BaseURL))
 	if err != nil {
 		panic(err)
