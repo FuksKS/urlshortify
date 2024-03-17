1fc39da6d9a63443683db3abbac21140eaa803f0
@fuks.kirill <fuks.kirill@wb.ru>
2024-03-18 00:45:02 +0400

    Add save cache for db

--- internal/pg/queries.go
+++ internal/pg/queries.go
@@ -18,4 +18,14 @@ create table if not exists shortener
 	getAllURLsQuery = `
 select short_url, original_url from shortener
 `
+	saveCashQuery = `
+insert into shortener (short_url, original_url)
+select
+    url_info.short_url,
+    url_info.original_url
+    from jsonb_to_recordset($1::jsonb) as url_info (
+                                                  "short_url" text,
+                                                  "original_url" text
+        ) on conflict do nothing;
+`
 )
--- internal/pg/rw.go
+++ internal/pg/rw.go
@@ -1,5 +1,34 @@
 package pg
 
+import (
+	"context"
+	"encoding/json"
+	"github.com/FuksKS/urlshortify/internal/models"
+	"time"
+)
+
 func (r *PgRepo) Save(cache map[string]string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	allURLs := make([]models.URLInfo, 0, len(cache))
+
+	for shortURL, originalURL := range cache {
+		allURLs = append(allURLs, models.URLInfo{
+			ShortURL:    shortURL,
+			OriginalURL: originalURL,
+		})
+	}
+
+	allURLsByte, err := json.Marshal(allURLs)
+	if err != nil {
+		return err
+	}
+
+	_, err = r.DB.Exec(ctx, saveCashQuery, string(allURLsByte))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
