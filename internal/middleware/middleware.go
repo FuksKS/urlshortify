d10acf2f86cbe8cd75322145702b6ce910d8bd4a
@fuks.kirill <fuks.kirill@wb.ru>
2024-07-30 23:05:54 +0400

    refactored auth middleware

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -143,21 +143,23 @@ func WithAuth(h http.Handler) http.Handler {
 		}
 
 		var userID string
-		if tokenWithUser.Value != "" {
-			logger.Log.Info("WithAuth middleware. tokenWithUser.Value != ''", zap.String(" tokenWithUser.Value: ", tokenWithUser.Value))
-			userID, err = token.GetUserID(tokenWithUser.Value)
-			logger.Log.Info("WithAuth middleware. token.GetUserID", zap.String("userID: ", userID))
-			if err != nil {
-				http.Error(w, "Get userID from token error", http.StatusInternalServerError)
-				return
-			}
-			if userID == "" {
+		if tokenWithUser != nil {
+			if tokenWithUser.Value != "" {
+				logger.Log.Info("WithAuth middleware. tokenWithUser.Value != ''", zap.String(" tokenWithUser.Value: ", tokenWithUser.Value))
+				userID, err = token.GetUserID(tokenWithUser.Value)
+				logger.Log.Info("WithAuth middleware. token.GetUserID", zap.String("userID: ", userID))
+				if err != nil {
+					http.Error(w, "Get userID from token error", http.StatusInternalServerError)
+					return
+				}
+				if userID == "" {
+					http.Error(w, "Unauthorized", http.StatusUnauthorized)
+					return
+				}
+			} else {
 				http.Error(w, "Unauthorized", http.StatusUnauthorized)
 				return
 			}
-		} else {
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-			return
 		}
 
 		if userID == "" {
